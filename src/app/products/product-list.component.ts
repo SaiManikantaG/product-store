901bb61d6a3d225d4d02ac5827eb123d90bfbd9f
SaiManikantaG <saig2304@gmail.com>
2018-04-15 21:28:30 -0500

    Calling product API using HTTP service and Dependecy Injection and applied bootstrap CSS

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -3,36 +3,35 @@ import { Iproduct } from './product';
 import { ProductService } from './product.service';
 
 @Component({
-    selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+  selector: 'pm-products',
+  templateUrl: './product-list.component.html',
 })
-export class ProductListComponent implements OnInit{
-    public pageTitle: string = 'Product List';
-    __listFilter: string;
-    get listFilter(){
-        return this.__listFilter;
-    }
-    set listFilter(value:string){
-        this.__listFilter = value;
-        this.filteredProducts=this.__listFilter?this.performFilter(this.listFilter):this.products;
-    }
-    constructor(private __productService: ProductService){
-        this.listFilter = '';
-    }
-    
+export class ProductListComponent implements OnInit {
+  public pageTitle: string = 'Product List';
+  __listFilter: string;
+  errorMessage: string;
+  get listFilter() {
+    return this.__listFilter;
+  }
+  set listFilter(value: string) {
+    this.__listFilter = value;
+    this.filteredProducts = this.__listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
+  constructor(private __productService: ProductService) {
+    this.listFilter = '';
+  }
 
-    performFilter(filterBy:string): Iproduct[]{
-        filterBy = filterBy.toLocaleLowerCase();
-        return this.products.filter((product:Iproduct)=>
-    product.name.toLocaleLowerCase().indexOf(filterBy) !== -1);
-    }
+  performFilter(filterBy: string): Iproduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: Iproduct) => product.name.toLocaleLowerCase().indexOf(filterBy) !== -1);
+  }
 
-    filteredProducts: Iproduct[];
-    products: Iproduct[] = [];
-    ngOnInit(): void{
-        this.products = this.__productService.getProducts();
-        this.filteredProducts = this.products;
-    }
-
-
+  filteredProducts: Iproduct[];
+  products: Iproduct[] = [];
+  ngOnInit(): void {
+    this.__productService.getProducts().subscribe(products => {
+      this.products = products;
+      this.filteredProducts = this.products;
+    }, error => (this.errorMessage = <any>error));
+  }
 }
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,36 +1,30 @@
 import { Injectable } from '@angular/core';
 import { Iproduct } from './product';
+import {HttpClient, HttpParameterCodec} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/do';
+import { HttpResponse } from 'selenium-webdriver/http';
+
+
 
 @Injectable()
 export class ProductService{
 
-    getProducts(): Iproduct[]{
-        return [
-            {
-                "name": "shirt",
-                "price": 15
-              },
-              {
-                "name": "basketball",
-                "price": 9.99
-              },
-              {
-                "name": "hat",
-                "price": 20
-              },
-              {
-                "name": "headphones",
-                "price": 49
-              },
-              {
-                "name": "pen",
-                "price": 1.99
-              },
-              {
-                "name": "sun glasses",
-                "price": 65
-              }
-        ]
+    private __productUrl = 'http://autumn-resonance-1298.getsandbox.com/products';
+    constructor(private __http:HttpClient){}
+
+    getProducts(): Observable<Iproduct[]>{
+        return this.__http.get<Iproduct[]>(this.__productUrl)
+        .do(data => console.log(`productList: ${JSON.stringify(data)}`))
+        .catch(this.handleError);
     }
 
+    private handleError(err: HttpResponse){
+        console.log(err.toString);
+        return Observable.throw(err.toString);
+
+
+    }
 }
