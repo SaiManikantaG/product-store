0bf7afee4e774cf61675913d12e74d4580f659f0
SaiManikantaG <saig2304@gmail.com>
2018-04-16 01:32:12 -0500

    Plan for creating rest API, PLAN B

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -34,10 +34,20 @@ export class ProductListComponent implements OnInit {
       this.filteredProducts = this.products;
     }, error => (this.errorMessage = <any>error));
 
+    // Throw it as a API endpoint????????
     var xpath = [];
     this.__productService.getProductInitial().subscribe(result => {
       this.products = xpath;
       console.log(`xpath:${xpath}`);
+      //productApi(xpath)
     });
+/* Build similiar to this?
+    var productApi = function(resp) {
+  
+        app.get('/api/products', function(req, res, body) {
+            console.log(`Products::${JSON.stringify(resp)}`);
+            res.status(200).send(resp);
+        }); 
+    };*/
   }
 }
